fb16b3accfa3146c91705dc5cbd12c2a766538d2
JonathanVanDyke <jfv213@gmail.com>
2020-05-31 18:29:40 -0400

    Add coords to grid

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
     [SerializeField][Range(1f, 20f)] float gridSize = 10;
-    void Awake()
+
+    TextMesh textMesh;
+
+    private void Start()
     {
-        Debug.Log("Editor causes this Awake");
+        textMesh = GetComponentInChildren<TextMesh>();
     }
 
     void Update()
     {
         Vector3 snapPos;
+
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize; // round causes binary switch
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridsize;
 
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
     }

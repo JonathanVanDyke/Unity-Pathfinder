c09b660e0983104e50afeb438d9b202ba3970fa4
JonathanVanDyke <jfv213@gmail.com>
2020-05-31 18:13:18 -0400

    Add custom editor snaps

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    [SerializeField][Range(1f, 20f)] float gridSize = 10;
+    void Awake()
+    {
+        Debug.Log("Editor causes this Awake");
+    }
+
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize; // round causes binary switch
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+    }
+}

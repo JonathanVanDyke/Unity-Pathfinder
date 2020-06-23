824219dbccc52d0af1934e6b09342bc8bbc6f51f
JonathanVanDyke <jfv213@gmail.com>
2020-06-23 08:27:10 -0400

    Add turret lookat and fire

---
+++ Assets/Tower.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] Transform objectToPan;
+    [SerializeField] Transform targetEnemy;
+    [SerializeField] float attackRange = 30f;
+    [SerializeField] ParticleSystem projectileParticle;
+
+    void Update()
+    {
+        if (targetEnemy)
+        {
+            objectToPan.LookAt(targetEnemy);
+            FireAtEnemy();
+        }
+        else
+        {
+            Shoot(false);
+        }
+       
+    }
+
+    private void FireAtEnemy()
+    {
+        float distanceToEnemy = Vector3.Distance(targetEnemy.transform.position, objectToPan.transform.position);
+        if (distanceToEnemy <= attackRange)
+        {
+            Shoot(true);
+        }
+        else
+        {
+            Shoot(false);
+        }
+    }
+
+    private void Shoot(bool isActive)
+    {
+        var emissionModule = projectileParticle.emission;
+        emissionModule.enabled = isActive;
+
+    }
+}
